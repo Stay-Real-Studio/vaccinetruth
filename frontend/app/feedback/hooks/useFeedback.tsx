fbf41799c1848ad84f391117bcada607986f55c5
Luke Zeng <haohao@stayreal.studio>
2023-12-30 09:01:52 -0800

    update feedback post

--- frontend/app/feedback/hooks/useFeedback.tsx
+++ frontend/app/feedback/hooks/useFeedback.tsx
@@ -8,8 +8,14 @@ export const useFeedback = (): SaveFeedback => {
     title: string | undefined,
     content: string | undefined
   ) => {
-    await sanityClient.patch("feedback").set({ title, content }).commit();
-    console.log(title, content);
+    const feedback = {
+      _type: 'feedback',
+      title,
+      content
+    }
+    const result = await sanityClient.create(feedback)
+    // .patch("feedback").set({ title, content }).commit();
+    console.log(result);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     // const feedbacks = await sanityClient.fetch('*[_type == "feedback"]');
     // console.log(feedbacks, "feedbacks");
