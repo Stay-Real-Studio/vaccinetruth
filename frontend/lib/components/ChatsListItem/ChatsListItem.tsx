0a7ed721838f0f03ca16093c6c61ea773cd09ca0
tianxiaxi <warren@stayreal.studio>
2024-01-15 23:29:14 +0800

    feat: chat list ui op

--- frontend/lib/components/ChatsListItem/ChatsListItem.tsx
+++ frontend/lib/components/ChatsListItem/ChatsListItem.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { FiEdit, FiSave, FiTrash2 } from "react-icons/fi";
-import { MdChatBubbleOutline } from "react-icons/md";
 
 import { ChatEntity } from "@/app/chat/[chatId]/types";
 import { cn } from "@/lib/utils";
@@ -43,8 +42,7 @@ export const ChatsListItem = ({
         href={`/chat/${chat.chat_id}`}
         key={chat.chat_id}
       >
-        <div className="flex items-center gap-2">
-          <MdChatBubbleOutline className="text-xl" />
+        <div className="flex items-center ">
           <ChatName
             setName={setChatName}
             editing={editingName}
@@ -52,24 +50,28 @@ export const ChatsListItem = ({
           />
         </div>
       </Link>
-      <div className="opacity-0 group-hover:opacity-100 flex items-center justify-center bg-gradient-to-l from-vt-50 dark:from-vt-700 to-transparent z-10 transition-opacity">
-        {editable && (
+      <div
+        className={`absolute z-10 right-1 ${editingName ? "-top-1" : "-top-1"}`}
+      >
+        <div className="opacity-0 group-hover:opacity-100 flex items-center justify-center bg-gradient-to-l from-vt-50 dark:from-vt-700 to-transparent z-10 transition-opacity">
+          {editable && (
+            <button
+              className="p-0 hover:text-blue-700 dark:text-vt-50 dark:hover:text-blue-700"
+              type="button"
+              onClick={handleEditNameClick}
+            >
+              {editingName ? <FiSave /> : <FiEdit />}
+            </button>
+          )}
           <button
-            className="p-0 hover:text-blue-700 dark:text-vt-50 dark:hover:text-blue-700"
+            className="p-2 hover:text-red-700 dark:text-vt-50 dark:hover:text-red-700"
             type="button"
-            onClick={handleEditNameClick}
+            onClick={onDelete ?? (() => void deleteChat())}
+            data-testid="delete-chat-button"
           >
-            {editingName ? <FiSave /> : <FiEdit />}
+            <FiTrash2 />
           </button>
-        )}
-        <button
-          className="p-2 hover:text-red-700 dark:text-vt-50 dark:hover:text-red-700"
-          type="button"
-          onClick={onDelete ?? (() => void deleteChat())}
-          data-testid="delete-chat-button"
-        >
-          <FiTrash2 />
-        </button>
+        </div>
       </div>
 
       {/* Fade to white */}
--- frontend/lib/components/ChatsListItem/components/ChatName.tsx
+++ frontend/lib/components/ChatsListItem/components/ChatName.tsx
@@ -15,10 +15,10 @@ export const ChatName = ({
         onChange={(event) => setName(event.target.value)}
         autoFocus
         value={name}
-        className="text-vt-700"
+        className="text-vt-700 pl-1 py-1"
       />
     );
   }
 
-  return <span className="max-w-[150px] truncate">{name}</span>;
+  return <span className="max-w-44 truncate">{name}</span>;
 };
