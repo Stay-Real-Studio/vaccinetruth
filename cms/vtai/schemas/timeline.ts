9b73f0aa44c50d375b04814ff6be0c1ec17a9fc1
tianxiaxi <warren@stayreal.studio>
2024-03-09 21:23:25 +0800

    feat: cms add timeline schema

---
+++ cms/vtai/schemas/timeline.ts
@@ -0,0 +1,58 @@
+export default {
+  name: 'timeline',
+  type: 'document',
+  title: '时间线',
+  fields: [
+    {
+      name: 'datetime',
+      title: 'Date',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        calendarTodayLabel: 'Today',
+      },
+    },
+    {
+      name: 'stage',
+      title: 'Stage',
+      type: 'number',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'object',
+      fields: [
+        {
+          name: 'zh_cn',
+          title: '简体中文',
+          type: 'text',
+        },
+        {
+          name: 'en',
+          title: 'English',
+          type: 'text',
+        },
+        {
+          name: 'ru',
+          title: 'Russian',
+          type: 'text',
+        },
+        {
+          name: 'es',
+          title: 'Spanish',
+          type: 'text',
+        },
+        {
+          name: 'ptbr',
+          title: 'Portuguese',
+          type: 'text',
+        },
+        {
+          name: 'fr',
+          title: 'French',
+          type: 'text',
+        },
+      ],
+    },
+  ],
+}
